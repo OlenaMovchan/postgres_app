bb130563f789953ff96534d662d12dc278346f14
Olena Movchan <elenawju@gmail.com>
2023-11-16 19:54:15 +0200

    add

--- src/main/java/com/shpp/query/QueryExecutor.java
+++ src/main/java/com/shpp/query/QueryExecutor.java
@@ -20,24 +20,24 @@ public class QueryExecutor {
     public  void querySQL(Connection connection) {
         String categoryName = System.getProperty("category", "Category_435");
 
-        String sql = "SELECT" +
-        "c.category_name,"
-                +"s.location AS store_location,"
-        +"SUM(d.product_count) AS total_products"
-       +"FROM"
-        +"categories c"
-        +"JOIN"
-        +"products p ON c.category_id = p.category_id"
-        +"JOIN"
-        +"deliveries d ON p.product_id = d.product_id"
-        +"JOIN"
-        +"stores s ON d.store_id = s.store_id"
-        +"WHERE"
-        +"c.category_name=?"
-        +"GROUP BY"
-        +"c.category_name, s.location"
-        +"ORDER BY"
-        +"total_products DESC"
+        String sql = "SELECT " +
+        "c.category_name, "
+                +"s.location AS store_location, "
+        +"SUM(d.product_count) AS total_products "
+       +"FROM "
+        +"categories c "
+        +"JOIN "
+        +"products p ON c.category_id = p.category_id "
+        +"JOIN "
+        +"deliveries d ON p.product_id = d.product_id "
+        +"JOIN "
+        +"stores s ON d.store_id = s.store_id "
+        +"WHERE "
+        +"c.category_name=? "
+        +"GROUP BY "
+        +"c.category_name, s.location "
+        +"ORDER BY "
+        +"total_products DESC "
         +"LIMIT 1";
         // sql = loadQueryFromFile();
         QueryResult result = null;
