37530c363d533e7fd02128c7fdae8fd2844126b6
Olena Movchan <elenawju@gmail.com>
2023-11-25 20:59:01 +0200

    ad

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -1,19 +1,36 @@
 SELECT
-    c.category_name,
-    s.location AS store_location,
-    SUM(d.product_count) AS total_products
+    s.location AS store_address,
+    COUNT(sp.product_id) AS product_count
 FROM
-    categories c
+    stores s
         JOIN
-    products p ON c.category_id = p.category_id
+    store_products sp ON s.store_id = sp.store_id
         JOIN
-    deliveries d ON p.product_id = d.product_id
-        JOIN
-    stores s ON d.store_id = s.store_id
+    products p ON sp.product_id = p.product_id
 WHERE
-        c.category_name = '?'
+        p.category_id = '?'
 GROUP BY
-    c.category_name, s.location
+    s.location
 ORDER BY
-    total_products DESC
+    product_count DESC
     LIMIT 1;
+
+--SELECT
+   -- c.category_name,
+    --s.location AS store_location,
+    --SUM(d.product_count) AS total_products
+--FROM
+    --categories c
+        --JOIN
+    --products p ON c.category_id = p.category_id
+        --JOIN
+   -- deliveries d ON p.product_id = d.product_id
+       -- JOIN
+   -- stores s ON d.store_id = s.store_id
+--WHERE
+        --c.category_name = '?'
+--GROUP BY
+    --c.category_name, s.location
+--ORDER BY
+   -- total_products DESC
+    --LIMIT 1;
